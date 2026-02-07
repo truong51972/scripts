bd6c97aea26287885599284f0c86d59f960813b3
truong51972 <tranquoctruong20@gmail.com>
2026-02-07 13:37:04 +0700

    update installers/zsh.sh

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -1,6 +1,8 @@
 sudo apt install zsh -y
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Set Zsh as the default shell
+chsh -s $(which zsh)
 
 # setup autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
