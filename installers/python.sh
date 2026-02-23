38bfb87b94de1a1988954ea9862caa67005ff742
truong51972 <tranquoctruong20@gmail.com>
2026-02-23 20:40:30 +0700

    update

--- installers/python.sh
+++ installers/python.sh
@@ -3,7 +3,8 @@ wget -qO- https://astral.sh/uv/install.sh | sh
 TITLE="# Alias for uv python"
 CONTENT=(
 'alias upython="uv run python"
-alias upip="uv run pip"'
+alias upip="uv run pip"
+alias uactive="source .venv/bin/activate"'
 )
 
 append_if_not_exists_in_script "$TITLE" "$CONTENT"
